be102055f1128f11e945d000d415b34e2314c9f5
RiicardoAC <106176120+RiicardoAC@users.noreply.github.com>
2022-10-11 11:29:13 -0500

    alterando a:hover

--- PHP/conexao.php
+++ PHP/conexao.php
@@ -1,10 +1,10 @@
 <?php
-    $servidor = "localhost";
-    $bdname = "cadastro";
-    $usuario = "root";
-    $senha = "";
+    $bd_servidor = "localhost";
+    $bd_name = "cadastro";
+    $bd_usuario = "root";
+    $$bd_senha = "";
 
-    $conexao = mysqli_connect($servidor, $usuario, $senha, $bdname);
+    $conexao = mysqli_connect($bd_servidor, $bd_usuario, $bd_senha, $bd_name);
 
     if(!$conexao)
       die("Problemas com a conexão com o banco de dados. Descrição do problema: " . mysqli_connect_error());
