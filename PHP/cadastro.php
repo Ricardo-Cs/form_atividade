66c5a7a96bda4131986027838c15475cc37238f2
RiicardoAC <106176120+RiicardoAC@users.noreply.github.com>
2022-10-11 12:03:42 -0500

    cadastro com banco de dados funcionando

--- PHP/cadastro.php
+++ PHP/cadastro.php
@@ -1,18 +1,29 @@
 <?php
-    include('conexao.php');
+    $bd_servidor = "localhost";
+    $bd_name = "cadastro";
+    $bd_usuario = "root";
+    $$bd_senha = "";
 
+    $conexao = mysqli_connect($bd_servidor, $bd_usuario, $bd_senha, $bd_name);
+
+    if(!$conexao)
+      die("Problemas com a conexão com o banco de dados. Descrição do problema: " . mysqli_connect_error());
+    echo "Conexão realizada com sucesso!";
+    //Conetando com o bando de dados
+
+    //Inserindo dados no banco
     $nome = $_POST['nome'];
     $senha = $_POST['senha'];
     $email = $_POST['email'];
     $telefone = $_POST['telefone'];
-    $sexo = ['sexo'];
+    $sexo = ['genero'];
     $data_nascimento = $_POST['data_nascimento'];
     $cidade = ['cidade'];
     $estado = ['estado'];
     $endereco = ['endereco'];
 
     
-    $sql = "INSERT INTO usuarios(nome, senha, email, telefone, nascimento, ) VALUES ('$nome', '$senha', '$email', '$telefone', '$data_nascimento')";
+    $sql = "INSERT INTO usuarios(nome, senha, email, telefone, genero, data_nascimento, estado, cidade, endereco) VALUES ('$nome', '$senha', '$email', '$telefone', '$sexo', '$data_nascimento', '$estado', '$cidade', '$endereco')";
 
     if(mysqli_query($conexao, $sql))
         echo 'Cadastrado  com sucesso';
